b548ab0d77e8cfe93cb6f7ce4d7b665bf213c98c
manajorell <37649610+manajorell@users.noreply.github.com>
2026-03-05 04:12:20 -0500

    Add files via upload

---
+++ ViewModelExt.kt
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="utf-8"?>
+<paths>
+    <external-files-path name="my_images" path="Pictures/" />
+    <cache-path name="shared_images" path="images/" />
+    <external-cache-path name="external_cache" path="." />
+</paths>
